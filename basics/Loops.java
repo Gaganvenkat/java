5a9126c95a0e2ef17f722c77fad3805cb341ebb2
Gagan Venkat <168264822+Gaganvenkat@users.noreply.github.com>
2025-08-12 14:49:40 +0530

    Create Loops.java
    loop example

---
+++ basics/Loops.java
@@ -0,0 +1,15 @@
+public class Loops {
+    public static void main(String[] args) {
+        // For loop
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+
+        // While loop
+        int count = 0;
+        while (count < 5) {
+            System.out.println("While count: " + count);
+            count++;
+        }
+    }
+}
