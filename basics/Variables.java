2f7765b68b447aaa6c2699384d1be401a276bbcb
Gagan Venkat <168264822+Gaganvenkat@users.noreply.github.com>
2025-08-12 14:49:05 +0530

    Create Variables.java
    variable example

---
+++ basics/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Gagan Venkat";
+        int age = 21;
+        double height = 5.85;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " ft");
+    }
+}
