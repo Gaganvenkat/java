5e0db0dec5f09f1270d2b77f69ca0fa37bdfc8e8
Gagan Venkat <168264822+Gaganvenkat@users.noreply.github.com>
2025-08-12 14:57:36 +0530

    Create MathExample.java
    using math in java

---
+++ intermediate/MathExample.java
@@ -0,0 +1,7 @@
+public class MathExample {
+    public static void main(String[] args) {
+        double radius = 5;
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.printf("Area of circle: %.2f%n", area);
+    }
+}
